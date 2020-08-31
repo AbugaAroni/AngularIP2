595fa371c9dd5b88aa1b9f0962c017e64897af80
AbugaAroni <abugaaroni@gmail.com>
2020-09-01 02:34:18 +0300

    add github token and create service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubApiToken: '44d2e2ca73b38603ec03f821692ca093c24ea4e4'
 };
 
 /*
