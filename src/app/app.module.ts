d8626e3443ce8865316ce65f4f9930fcaf64d817
AbugaAroni <abugaaroni@gmail.com>
2020-09-01 03:56:13 +0300

    add a the search bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubUsersComponent } from './github-users/github-users.component';
+import { GithubSearchComponent } from './github-search/github-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubUsersComponent
+    GithubUsersComponent,
+    GithubSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/github-search/github-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubapiService } from '../githubapi.service'
+
+@Component({
+  selector: 'app-github-search',
+  templateUrl: './github-search.component.html',
+  styleUrls: ['./github-search.component.css']
+})
+export class GithubSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
