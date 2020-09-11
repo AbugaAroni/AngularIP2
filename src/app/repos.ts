8a92043f72619d072beb725f2b00ef3958b88966
AbugaAroni <abugaaroni@gmail.com>
2020-09-11 11:27:01 +0300

    add working user and repository classes

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,4 +1,10 @@
 export class Repos {
   constructor(
-    public name:any) {}
+    public name:any,
+    public description:any,
+    public html_url:any,
+    public forks_count:any,
+    public language:any,
+    public updated_at:any,
+  ) {}
 }
--- src/app/users.ts
+++ src/app/users.ts
@@ -2,5 +2,15 @@ export class Users {
     constructor(
       public login:any,
       public name:any,
-      public avatar_url: any) {}
+      public avatar_url: any,
+      public html_url: any,
+      public company: any,
+      public location: any,
+      public email: any,
+      public followers: any,
+      public following: any,
+      public public_repos: any,
+      public bio: any,
+      public blog: any,
+      public created_at: any) {}
 }
